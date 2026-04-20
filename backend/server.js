7c6c9cc5906489e5b49aed86f61015003a582490
Minal Meshram <minalmeshram873@gmail.com>
2026-04-20 16:49:13 +0530

    backend ready

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use("/api/contact", contactRoutes);
 
 
 // DB connection
-mongoose.connect("mongodb://localhost:27017/portfolioDB")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
